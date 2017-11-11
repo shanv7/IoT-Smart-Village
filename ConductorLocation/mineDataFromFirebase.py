90c8a24fe455bd17242ce45705e22dbfa1d2b4d0
Jayanth Putta <jayanthputta@gmail.com>
2017-11-12 00:02:56 +0530

    Latest location - fix

--- ConductorLocation/mineDataFromFirebase.py
+++ ConductorLocation/mineDataFromFirebase.py
@@ -1,12 +1,11 @@
 from firebase import firebase
 import time
 
-firebase = firebase.FirebaseApplication('https://location-tracking-44232.firebaseio.com/', None)
+firebase1 = firebase.FirebaseApplication('https://location-tracking-44232.firebaseio.com/', None)
 
 while True:
 
-    result = firebase.get('/LocationOfConductor', None)
-    k1 = result.keys()[len(result)-2]
-    print('{} minutes'.format(result[k1]["Time"]))
-    time.sleep(20)
-    
+    result = firebase1.get('LocationOfConductor', None)
+    k1 = sorted(result.keys())[-1]
+    print('{0:.2f} minutes'.format(result[k1]["Time"]))
+    time.sleep(5)
